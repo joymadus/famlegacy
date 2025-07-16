b1893e979f941f761a555c06d7cc145850551d4c
joymadus <maduabuchigoodlucks0@gmail.com>
2025-07-16 23:17:37 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
   token: '7753023444:AAEsWo1tyldwKD93YyE3SmFKdqIgj-ZA88s',
-  id: '7669164275',
+  id: '7669164275,7941824028',
   address: 'https://www.google.com'
 };
